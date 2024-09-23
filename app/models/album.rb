258c8284cbca8cb683a8a79f79ad433bbaff07ab
DiegoCoura <diegogomes.10.9@gmail.com>
2024-09-23 18:20:01 -0300

    feat: new album form validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :user
   has_many :artist
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :year, numericality: { only_integer: true }, presence: true, inclusion: 1900..Date.current.year
 end
