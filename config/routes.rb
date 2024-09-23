eb8e7a2f303a7c5c4f576471bd91529d1924b625
DiegoCoura <diegogomes.10.9@gmail.com>
2024-09-23 15:05:06 -0300

    feat: add albums#index as home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root "albums#index"
+
   get "/albums", to: "albums#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
