6d712b6dcbce0df7d9b1451bf95babf99e8f9e2a
DiegoCoura <diegogomes.10.9@gmail.com>
2024-09-23 22:42:17 -0300

    feat: migration to add user foreign key into album

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_23_153130) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_24_013546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_23_153130) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
   create_table "artists", id: false, force: :cascade do |t|
@@ -42,4 +44,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_23_153130) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "albums", "users"
 end
